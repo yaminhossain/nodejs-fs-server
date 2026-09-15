fda998f5aac733b6147c24b4e5b25efab1f19db9
yaminhossain <yaminhossain.work@gmail.com>
2026-09-15 16:20:38 +0600

    fix: missing data directory and response header race condition

--- src/server.js
+++ src/server.js
@@ -21,38 +21,70 @@ const server = http.createServer((req, res) => {
     req.on("end", () => {
       // read file from the storage
       const apiDataObj = JSON.parse(apiData);
-      fs.readFile(savedLocation, { encoding: "utf8" }, (err, data) => {
-        if (err || !data) {
-          fs.writeFile(
-            savedLocation,
-            JSON.stringify([apiDataObj], null, 2),
-            (err) => {
-              console.log("Error Writing File", err);
-            },
-          );
-        } else {
-          const parsedData = JSON.parse(data);
-          parsedData.push(apiDataObj);
-          fs.writeFile(
-            savedLocation,
-            JSON.stringify(parsedData, null, 2),
-            (err) => {
-              console.log("Error Writing File", err);
-            },
-          );
+      // handling non-existence folder error
+      fs.mkdir(path.dirname(savedLocation), { recursive: true }, (err) => {
+        if (err) {
+          console.log("There was an error while creating the directory:", err);
+          return;
         }
+        fs.readFile(savedLocation, { encoding: "utf8" }, (err, data) => {
+          if (err || !data) {
+            fs.writeFile(
+              savedLocation,
+              JSON.stringify([apiDataObj], null, 2),
+              (err) => {
+                if (err) {
+                  console.log("Error Writing File", err);
+                  return;
+                }
+
+                // response headers
+                res.writeHead(200, "OK", {
+                  "content-type": "application/json",
+                });
+                res.end(
+                  JSON.stringify(
+                    {
+                      status: "OK",
+                      data: apiDataObj,
+                    },
+                    null,
+                    2,
+                  ),
+                );
+              },
+            );
+          } else {
+            const parsedData = JSON.parse(data);
+            parsedData.push(apiDataObj);
+            fs.writeFile(
+              savedLocation,
+              JSON.stringify(parsedData, null, 2),
+              (err) => {
+                if (err) {
+                  console.log("Error Writing File", err);
+                  return;
+                }
+
+                // response headers
+                res.writeHead(200, "OK", {
+                  "content-type": "application/json",
+                });
+                res.end(
+                  JSON.stringify(
+                    {
+                      status: "OK",
+                      data: apiDataObj,
+                    },
+                    null,
+                    2,
+                  ),
+                );
+              },
+            );
+          }
+        });
       });
-      res.writeHead(200, "OK", { "content-type": "application/json" });
-      res.end(
-        JSON.stringify(
-          {
-            status: "OK",
-            data: apiDataObj,
-          },
-          null,
-          2,
-        ),
-      );
     });
   }
 });
