0f38583fb7c8197ee4b882811b89d660ef2dad58
yaminhossain <yaminhossain.work@gmail.com>
2026-09-17 15:55:34 +0600

    fix: upadated patch method logic

--- src/controllers/updateTodo.js
+++ src/controllers/updateTodo.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
 const updateTodo = (req, res, id, savedLocation) => {
-  console.log(" ===========Update Function Hit======== ");
+  console.log(" ############ Update Function Hit ############ ");
 
   let data = "";
   // Reading Data from request body
@@ -12,142 +12,133 @@ const updateTodo = (req, res, id, savedLocation) => {
   req.on("end", () => {
     const parsedApiData = JSON.parse(data);
     const { set = {}, upsert = false } = parsedApiData;
+    const sanitized = isSanitized(set, upsert);
 
-    // Reading file
-    fs.readFile(savedLocation, { encoding: "utf8" }, (err, data) => {
-      if (err || !data) {
-        res.writeHead(404, "Not Found", {
-          "content-type": "application/json",
-        });
-        res.end(
-          JSON.stringify({
-            status: "error",
-            message: "Nothing to update",
-          }),
-        );
-        return;
-      }
-
-      const parsedObj = JSON.parse(data);
-      const matchedTodo = parsedObj.find(
-        (obj) => Number(obj.id) === Number(id),
+    console.log("Is Set and Upsert sanitized:::::::", sanitized);
+    // set object must contain some properties
+    if (Object.keys(set).length === 0) {
+      res.writeHead(405, "Not Allowed", {
+        "content-type": "application/json",
+      });
+      res.end(
+        JSON.stringify({
+          status: "error",
+          message: "set object must contain some properties",
+        }),
       );
-
-      // Test Case 01
-      if (
-        matchedTodo &&
-        upsert &&
-        typeof upsert === "boolean" &&
-        // set instanceof Object
-        // typeof set === "object"
-        isPlainObject(set)
-      ) {
-        for (let key in set) {
-          matchedTodo[key] = set[key];
-        }
-        // console.log("Stringified New data ===>", JSON.stringify(parsedObj));
-
-        fs.writeFile(
-          savedLocation,
-          JSON.stringify(parsedObj, null, 2),
-          (err) => {
-            if (err) {
-              res.writeHead(500, "Internal Server Error", {
-                "content-type": "application/json",
-              });
-
-              res.end(
-                JSON.stringify({
-                  status: "error",
-                  message: "server error",
-                }),
-              );
-              return;
-            }
-            // Response For matched data
-            res.writeHead(200, "OK", {
-              "content-type": "application/json",
-            });
-            res.end(
-              JSON.stringify({
-                status: "OK",
-                message: "আপডেট করেছি ",
-              }),
-            );
-            return;
-          },
-        );
-      }
-
-      // Test Case 02
-      else if (
-        matchedTodo &&
-        !upsert &&
-        typeof upsert === "boolean" &&
-        // upsert instanceof Boolean &&
-        // typeof set === "object"
-        isPlainObject(set)
-      ) {
-        const setKeys = Object.keys(set);
-        const matchedTodoKeys = Object.keys(matchedTodo);
-        const temp = [];
-        setKeys.forEach((key) => {
-          matchedTodoKeys.forEach((mKey) => {
-            // if(key in matchedKeys)
-            if (key === mKey) {
-              temp.push(key);
-            }
-          });
-        });
-
-        temp.forEach((key) => {
-          matchedTodo[key] = set[key];
-        });
-
-        // console.log("Parsed Object After Update =======>", parsedObj);
-
-        const updatedTodo = JSON.stringify(parsedObj, null, 2);
-
-        fs.writeFile(savedLocation, updatedTodo, (err) => {
-          if (err) {
-            res.writeHead(500, "Internal Server Error", {
-              "content-type": "application/json",
-            });
-
-            res.end(
-              JSON.stringify({
-                status: "error",
-                message: "server error",
-              }),
-            );
-            return;
-          }
-
-          // Response For matched data
-          res.writeHead(200, "OK", {
+      return;
+    } else {
+      // Reading file
+      fs.readFile(savedLocation, { encoding: "utf8" }, (err, fileData) => {
+        // File doesn't exist or no data inside the file
+        if (err || !fileData) {
+          res.writeHead(404, "Not Found", {
             "content-type": "application/json",
           });
           res.end(
             JSON.stringify({
-              status: "OK",
-              message: "আপডেট করেছি ",
+              status: "error",
+              message: "Nothing to update",
             }),
           );
           return;
-        });
-      } else {
-        res.writeHead(404, "Not Found", {
-          "content-type": "application/json",
-        });
+        }
+
+        // -------------------- If data exist -------------------
+        const parsedObj = JSON.parse(fileData);
+        const matchedTodo = parsedObj.find(
+          (obj) => Number(obj.id) === Number(id),
+        );
+
+        if (upsert && !matchedTodo && sanitized) {
+          set.id = id;
+          parsedObj.push(set);
+          const updatedTodo = JSON.stringify(parsedObj, null, 2);
+          fs.writeFile(savedLocation, updatedTodo, (err) => {
+            if (err) {
+              res.writeHead(500, "Internal Server Error", {
+                "content-type": "application/json",
+              });
+              res.end(
+                JSON.stringify({
+                  status: "error",
+                  message:
+                    "Although upsert: false, matchedTodo: false and data sanitized, something went wrong",
+                }),
+              );
+              return;
+            }
+            res.writeHead(200, "OK", { "content-type": "application/json" });
+            res.end(
+              JSON.stringify({
+                status: "OK",
+                message: `Successfully updated data for id${id}`,
+              }),
+            );
+          });
+          return;
+        }
+
+        if (!upsert && !matchedTodo && sanitized) {
+          res.writeHead(404, "Not Found", {
+            "content-type": "application/json",
+          });
+          res.end(
+            JSON.stringify({
+              status: "error",
+              message: `Id: ${id} did not matched`,
+            }),
+          );
+          return;
+        }
+
+        if (matchedTodo && sanitized) {
+          const setKeys = Object.keys(set);
+          const matchedObjKeys = Object.keys(matchedTodo);
+          const temp = [];
+
+          setKeys.forEach((sk) =>
+            matchedObjKeys.forEach((mk) => mk === sk && temp.push(sk)),
+          );
+
+          temp.forEach((tk) => (matchedTodo[tk] = set[tk]));
+
+          const updatedTodo = JSON.stringify(parsedObj, null, 2);
+          fs.writeFile(savedLocation, updatedTodo, (err) => {
+            if (err) {
+              res.writeHead(500, "Internal server error", {
+                "content-type": "application/json",
+              });
+              res.end(
+                JSON.stringify({
+                  status: "error",
+                  message: "Something went wrong",
+                }),
+              );
+              return;
+            }
+
+            res.writeHead(200, "OK", { "content-type": "application/json" });
+            res.end(
+              JSON.stringify({
+                status: "OK",
+                message: `Successfully updated data for ${id}`,
+              }),
+            );
+          });
+          return;
+        }
+
+        // Ultimate failure
+        res.writeHead(403, "Forbidden", { "content-type": "application/json" });
         res.end(
           JSON.stringify({
             status: "error",
-            message: "Nothing to update",
+            message: "Please enter valid data",
           }),
         );
-        return;
-      }
-    });
+      });
+    }
   });
 };
 
@@ -159,4 +150,8 @@ function isPlainObject(val) {
     typeof val === "object" &&
     Object.getPrototypeOf(val) === Object.prototype
   );
+}
+
+function isSanitized(set, upsert) {
+  return isPlainObject(set) && typeof upsert === "boolean";
 }
